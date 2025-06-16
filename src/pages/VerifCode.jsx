79e65fcc251d444817ddb46a4828fbb67803866c
Plagaulix <planquette.guillaume@gmail.com>
2025-06-16 11:22:28 +0200

    correction style

--- src/pages/VerifCode.jsx
+++ src/pages/VerifCode.jsx
@@ -9,17 +9,17 @@ const VerifCode = () => {
             <br></br>
             <br></br>
             <br></br>
-            <div class="container mt-3">
-                <div class="mx-auto" style="max-width: 600px;">
+            <div className="container mt-3">
+                <div className="mx-auto" style={{maxwidth: "600px"}}>
                     <br></br> <br></br> <br></br>
                     <h2>Saisisez le code re�u par email</h2>
                     <form action="/home.html">
-                        <div class="mb-3 mt-3">
+                        <div className="mb-3 mt-3">
                             <label for="code">Code � 6 chiffres.</label> <input type="number"
-                                class="form-control" id="code" placeholder="Enter code"
+                                className="form-control" id="code" placeholder="Enter code"
                                 name="code"></input>
                         </div>
-                        <a href="set_pwd.html" class="btn btn-primary">Continuer</a>
+                        <a href="set_pwd.html" className="btn btn-primary">Continuer</a>
                     </form>
                 </div>
             </div>
