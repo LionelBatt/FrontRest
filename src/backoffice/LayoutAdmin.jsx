1c5abc6035cf5cd745c02027805a54f1e53f735b
Plagaulix <165595388+Plagaulix@users.noreply.github.com>
2025-06-19 10:25:57 +0200

    Update LayoutAdmin.jsx

--- src/backoffice/LayoutAdmin.jsx
+++ src/backoffice/LayoutAdmin.jsx
@@ -1,20 +1,13 @@
-
 import { Outlet } from "react-router-dom";
 import LoadingBar from "../components/LoadingBar";
 import HeaderAdmin from "./HeaderAdmin";
-import React, { useState } from 'react';
-import CrudT from './CrudT';
-import CrudU from './CrudU';
 
 const LayoutAdmin = () => {
-  const [view, setView] = useState('trips');
   return (
     <>
-      <HeaderAdmin setView={setView} />
+      <HeaderAdmin />
       <LoadingBar />
       <main>
-        {view === 'trips' && <CrudT />}
-        {view === 'users' && <CrudU />}
         <Outlet />
       </main>
     </>
